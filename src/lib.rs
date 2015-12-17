71886db5baf1aa4c44584e13e4b5d0f39509154a
Mike Tang <miketang84@outlook.com>
2015-12-17 15:47:27 +0800

    adjust format.
    Signed-off-by: Mike Tang <miketang84@outlook.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 extern crate mustache;
 extern crate rustc_serialize;
 
-use std::io::Write;
 use std::path::Path;
 use rustc_serialize::Encodable;
 
 pub struct View;
 
 impl View {
-	pub fn render<T: Encodable> (pathstr: &str, data: T) -> Vec<u8> {
+	pub fn render<T: Encodable> (pathstr: &str, data: T) -> String {
 		let path = Path::new(pathstr);
-                let template = mustache::compile_path(path).unwrap();
-                let mut output: Vec<u8> = vec![];
-                
-                template.render(&mut output, &data).unwrap();
+        let template = mustache::compile_path(path).unwrap();
+        let mut output: Vec<u8> = vec![];
         
-                output
+        template.render(&mut output, &data).unwrap();
+
+        String::from_utf8(output).expect(&format!("failed to render template {}", pathstr))
 	}
 }
 
@@ -31,5 +30,5 @@ fn it_works() {
 
     //let view_instance = View;
     let output =  View::render("test.txt", hash);
-    assert!(String::from_utf8(output).unwrap() == "abcdefg hello world! higklmn.");
+    assert!(output == "abcdefg hello world! higklmn.");
 }
